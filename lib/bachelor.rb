f0f83f7ade91b0b5c885e844e28520ae833e7549
jjhiggz <jonathan.higger@gmail.com>
2020-02-20 16:52:10 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,35 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].find() {|n|n["status"]=="Winner"}["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  allSeasons = data.values.flat_map{|n| n}
+  allSeasons.reduce(0) do |sum, n |
+    if n["occupation"]==occupation
+        sum = n["name"]
+    else sum = sum
+    end
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  allSeasons = data.values.flat_map{|n| n}
+  allSeasons.reduce(0) do |sum, n|
+    if n["hometown"] == hometown
+      sum = sum + 1
+    else 
+      sum = sum
+    end
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  allSeasons = data.values.flat_map{|n| n}
+  allSeasons.find{|n|n["hometown"]==hometown}["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  allSeasons = data.values.flat_map{|n| n}
+   allSeasons.reduce{|sum, n| sum=sum+n["age"].to_i}
 end
